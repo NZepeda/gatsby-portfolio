2de2102ae43aebb4715551eb82072b7538c50538
Nestor Zepeda <NestorDZepeda@gmail.com>
2020-02-07 00:02:46 -0500

    Intro section with blurb and image

--- src/components/image.js
+++ src/components/image.js
@@ -16,17 +16,17 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      meImage: file(relativePath: { eq: "me.png" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+          sizes(maxWidth: 2500) {
+            ...GatsbyImageSharpSizes
           }
         }
       }
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <Img fluid={data.meImage.childImageSharp.sizes} />
 }
 
 export default Image
---
+++ src/sections/intro.js
@@ -0,0 +1,31 @@
+import React from "react"
+import Image from "../components/image"
+
+const fontStyle = {
+  fontFamily: "Muli",
+  fontWeight: "900",
+  fontSize: "3em",
+  lineHeight: "1.2em",
+}
+
+const IntroBlurb = () => (
+  <div>
+    <span style={fontStyle}>Hi there, I'm Nestor.</span>
+    <br />
+    <span style={{ ...fontStyle, color: "#4C8D8D" }}>Software Engineer,</span>
+    <br />
+    <span style={fontStyle}>Coffee Enthusiast</span>
+    <br />
+  </div>
+)
+
+const Intro = () => {
+  return (
+    <div>
+      <IntroBlurb />
+      <Image />
+    </div>
+  )
+}
+
+export default Intro
